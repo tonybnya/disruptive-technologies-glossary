15ce2ff515cd4b5c7449eba63ec93c9fb41fe541
tonybnya <nya.tony2010@gmail.com>
2024-08-24 17:37:54 +0100

    feat: redefine the app.py file by creating a create_app function

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,11 @@ This file creates the application.
 from __future__ import annotations
 
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-# Create a Flask application
-app: Flask = Flask(__name__)
+# Define a database object
+db: SQLAlchemy = SQLAlchemy()
 
 
 @app.route("/")
@@ -21,5 +23,24 @@ def root() -> dict[str, str]:
     return {"message": "Welcome! This is a Glossary on AI and Blockchain"}
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5003)
+def create_app() -> Flask:
+    """
+    Create the Flask app and return it as an object.
+
+    Input:  Nothing
+    Output: an object representing the Flask application
+    """
+    # Define the Flask application
+    app: Flask = Flask(__name__)
+
+    # Define a string for the SQLite database
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./glossary.db"
+
+    # Initialize the Flask application
+    db.init_app(app)
+
+    # TODO: import later on
+
+    migrate: Migrate = Migrate(app, db)
+
+    return app
--- backend/run.py
+++ backend/run.py
@@ -1,3 +1,33 @@
 """
 This file runs the application.
 """
+
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+# Define a database object
+db: SQLAlchemy = SQLAlchemy()
+
+
+def create_app() -> Flask:
+    """
+    Create the Flask app and return it as an object.
+
+    Input:  Nothing
+    Output: an object representing the Flask application
+    """
+    # Define the Flask application
+    app: Flask = Flask(__name__)
+
+    # Define a string for the SQLite database
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./glossary.db"
+
+    # Initialize the Flask application
+    db.init_app(app)
+
+    # TODO: import later on
+
+    migrate: Migrate = Migrate(app, db)
+
+    return app
