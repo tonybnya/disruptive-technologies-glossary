0c8dfae251436dadf27189494777b60bcfa1ffc2
tonybnya <nya.tony2010@gmail.com>
2024-08-24 17:43:06 +0100

    feat: define the content of run.py file to run the Flask app in debug mode

--- backend/run.py
+++ backend/run.py
@@ -2,32 +2,14 @@
 This file runs the application.
 """
 
+from __future__ import annotations
+
 from flask import Flask
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
 
-# Define a database object
-db: SQLAlchemy = SQLAlchemy()
+from app import create_app
 
+flask_app: Flask = create_app()
 
-def create_app() -> Flask:
-    """
-    Create the Flask app and return it as an object.
-
-    Input:  Nothing
-    Output: an object representing the Flask application
-    """
-    # Define the Flask application
-    app: Flask = Flask(__name__)
-
-    # Define a string for the SQLite database
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./glossary.db"
-
-    # Initialize the Flask application
-    db.init_app(app)
-
-    # TODO: import later on
-
-    migrate: Migrate = Migrate(app, db)
-
-    return app
+if __name__ == "__main__":
+    # Run the application in debug mode
+    flask_app.run(host="0.0.0.0", debug=True, port=5003)
