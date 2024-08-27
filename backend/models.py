3c2cf85753fc86fd81acee10baea0a14829c11f4
tonybnya <nya.tony2010@gmail.com>
2024-08-27 17:19:31 +0100

    update models

--- backend/models.py
+++ backend/models.py
@@ -4,6 +4,8 @@ This file contains the models for the database.
 
 from __future__ import annotations
 
+from typing import Dict, List
+
 from app import db
 
 
@@ -13,6 +15,11 @@ class Term(db.Model):
     Each Term in the glossary is represented with both English & French equivalents.
 
     Attributes:
+        domain (str): The domain to which all terms belong.
+
+        subdomains (list): Subdomains to which the term belongs.
+                           If not specified, belongs to 'Big Data', 'AI', and 'Blockchain'.
+
         tid (int): The primary key for the term.
 
         english_term (str): The English term.
@@ -21,32 +28,43 @@ class Term(db.Model):
         variant_en (str): Variant of the term in English.
         variant_fr (str): Variant of the term in French.
 
-        synonyms_en (str): Synonyms of the term in English.
-        synonyms_fr (str): Synonyms of the term in French.
+        near_synonym_en (str): Near synonym of the term in English.
+        near_synonym_fr (str): Near synonym of the term in French.
 
         definition_en (str): Definition of the term in English.
         definition_fr (str): Definition of the term in French.
 
-        syntactic_cooccurrence_en (str): Syntactic cooccurrence information in English.
-        syntactic_cooccurrence_fr (str): Syntactic cooccurrence information in French.
+        syntactic_cooccurrence_en (list): Syntactic cooccurrence information in English.
+        syntactic_cooccurrence_fr (list): Syntactic cooccurrence information in French.
 
-        lexical_relations_en (str): Lexical relationships in English.
-        lexical_relations_fr (str): Lexical relationships in French.
+        lexical_relations_en (dict): Lexical relationships in English.
+        lexical_relations_fr (dict): Lexical relationships in French.
+
+        note_en (str): Note about the term in English.
+        note_fr (str): Note about the term in French.
+
+        not_to_be_confused_with_en (str): expression not to be confused with the term in English.
+        not_to_be_confused_with_fr (str): expression not to be confused with the term in French.
+
+        frequent_expression_en (str): Frequent expressions in English.
+        frequent_expression_fr (str): Frequent expressions in French.
 
         phraseology_en (str): Phraseology information in English.
         phraseology_fr (str): Phraseology information in French.
 
-        related_term_en (str): Related term in English.
-        related_term_fr (str): Related term in French.
-
-        contexts_en (str): Contexts in English.
-        contexts_fr (str): Contexts in French.
-
-        frequent_expression_en (str): Frequent expressions in English.
-        frequent_expression_fr (str): Frequent expressions in French.
+        context_en (str): Context in English.
+        context_fr (str): Context in French.
     """
 
+    # Define the name of the table in the database
     __tablename__ = "terms"
+
+    # Class attribute
+    # Each single term in the database belongs to this domain
+    domain: str = "Technologies Transformatrices"
+
+    # New class attribute for subdomains
+    subdomains: List[str] = ["Big Data", "AI", "Blockchain"]
 
     tid: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
@@ -56,29 +74,32 @@ class Term(db.Model):
     variant_en: str = db.Column(db.String(255))
     variant_fr: str = db.Column(db.String(255))
 
-    synonyms_en: str = db.Column(db.String(255))
-    synonyms_fr: str = db.Column(db.String(255))
+    near_synonym_en: str = db.Column(db.String(255))
+    near_synonym_fr: str = db.Column(db.String(255))
 
     definition_en: str = db.Column(db.Text)
     definition_fr: str = db.Column(db.Text)
 
-    syntactic_cooccurrence_en: str = db.Column(db.Text)
-    syntactic_cooccurrence_fr: str = db.Column(db.Text)
+    syntactic_cooccurrence_en: List[str] = db.Column(db.JSON)
+    syntactic_cooccurrence_fr: List[str] = db.Column(db.JSON)
 
-    lexical_relations_en: str = db.Column(db.Text)
-    lexical_relations_fr: str = db.Column(db.Text)
+    lexical_relations_en: Dict[str, List[str]] = db.Column(db.JSON)
+    lexical_relations_fr: Dict[str, List[str]] = db.Column(db.JSON)
+
+    note_en: str = db.Column(db.Text)
+    note_fr: str = db.Column(db.Text)
+
+    not_to_be_confused_with_en: str = db.Column(db.String(255))
+    not_to_be_confused_with_fr: str = db.Column(db.String(255))
+
+    frequent_expression_en: str = db.Column(db.Text)
+    frequent_expression_fr: str = db.Column(db.Text)
 
     phraseology_en: str = db.Column(db.Text)
     phraseology_fr: str = db.Column(db.Text)
 
-    related_term_en: str = db.Column(db.String(255))
-    related_term_fr: str = db.Column(db.String(255))
-
-    contexts_en: str = db.Column(db.Text)
-    contexts_fr: str = db.Column(db.Text)
-
-    frequent_expression_en: str = db.Column(db.Text)
-    frequent_expression_fr: str = db.Column(db.Text)
+    context_en: str = db.Column(db.Text)
+    context_fr: str = db.Column(db.Text)
 
     __table_args__ = (
         db.UniqueConstraint("english_term", "french_term", name="unique_terms"),
