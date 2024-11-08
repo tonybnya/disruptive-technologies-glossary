51e5961943a419189953449934ccbe4ad3acae1e
tonybnya <nya.tony2010@gmail.com>
2024-11-08 17:35:31 +0100

    style: adjust the padding-x of the main content of the glossary page

--- static/js/searchTerm.js
+++ static/js/searchTerm.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const searchInput = document.getElementById("search-input");
   const searchButton = document.getElementById("search-button");
   const resultsContainer = document.getElementById("results");
